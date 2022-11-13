d6e2bdd2c810198bb27f0e24f5f943886c55586a
Ágata Ordano <aordano@protonmail.com>
2022-11-13 18:25:22 -0300

    refactor(model): refactor device into own modulé and errors using `thiserror` crate

--- src/model.rs
+++ src/model.rs
@@ -1,6 +1,4 @@
-use super::wiz_errors::{
-    DeviceColorTempParseError, DeviceTypeParseError, SceneIDError, SceneNameError,
-};
+use super::wiz_errors::{DeviceError, SceneError};
 use anyhow::Error;
 use bytemuck::{cast, try_cast};
 use chrono::Duration;
@@ -14,7 +12,7 @@ use std::collections::HashMap;
 use uuid::Uuid;
 
 lazy_static! {
-    static ref SCENES: HashMap<&'static str, u32> = HashMap::from([
+    pub static ref SCENES: HashMap<&'static str, u32> = HashMap::from([
         ("Ocean", 1),
         ("Romance", 2),
         ("Sunset", 3),
@@ -49,7 +47,7 @@ lazy_static! {
         ("Steampunk", 32),
         ("Rhythm", 1000),
     ]);
-    static ref DEVICE_OPTS: DeviceOptions = DeviceOptions {
+    pub static ref DEVICE_OPTS: DeviceOptions = DeviceOptions {
         rgb: "RGB",
         dimmable_white: "DW",
         tunable_white: "TW",
@@ -57,8 +55,8 @@ lazy_static! {
         single_head: "SH",
         socket: "SOCKET"
     };
-    static ref KNOWN_TYPE_IDS: Vec<DeviceType> = vec![DeviceType::BulbDW];
-    static ref WIZMOTE_BUTTON_MAP: HashMap<&'static str, &'static str> = HashMap::from([
+    pub static ref KNOWN_TYPE_IDS: Vec<DeviceType> = vec![DeviceType::BulbDW];
+    pub static ref WIZMOTE_BUTTON_MAP: HashMap<&'static str, &'static str> = HashMap::from([
         ("wfa1", "on"),
         ("wfa2", "off"),
         ("wfa3", "night"),
@@ -69,9 +67,9 @@ lazy_static! {
         ("wfa18", "3"),
         ("wfa19", "4"),
     ]);
-    static ref RESPONSE_PORT: u16 = 38899;
-    static ref LISTENING_PORT: u16 = 38900;
-    static ref DEFAULT_NETWORK_CONFIG: NetworkConfig = NetworkConfig {
+    pub static ref RESPONSE_PORT: u16 = 38899;
+    pub static ref LISTENING_PORT: u16 = 38900;
+    pub static ref DEFAULT_NETWORK_CONFIG: NetworkConfig = NetworkConfig {
         timeout: Duration::seconds(13),
         max_sent_datagrams: 6,
         first_send_interval: Duration::milliseconds(750),
@@ -129,22 +127,16 @@ pub enum Scenes {
 }
 
 impl Scenes {
-    pub fn from_id(id: u32) -> Result<Self, Error> {
+    pub fn from_id(id: u32) -> Result<Self, SceneError> {
         let scene: Option<Self> = FromPrimitive::from_u32(id);
-        return scene.ok_or(Error::new(SceneIDError {
-            given_id: id,
-            details: "Scene ID out of range. Expected 1-32 or 1000".into(),
-        }));
+        return scene.ok_or(SceneError::IDError(id));
     }
 
-    pub fn from_name(name: String) -> Result<Self, Error> {
+    pub fn from_name(name: String) -> Result<Self, SceneError> {
         let scene_name = name.clone();
         let id = SCENES
             .get::<&str>(&name.as_str())
-            .ok_or(Error::new(SceneNameError {
-                given_name: scene_name.into(),
-                details: "Scene name not found.".into(),
-            }))?;
+            .ok_or(SceneError::NameError(name.clone()))?;
         return Scenes::from_id(*id);
     }
 
@@ -596,410 +588,18 @@ pub enum DeviceType {
 }
 
 #[derive(Debug, Clone)]
-pub enum Device {
-    Bulb(Bulb),
-    Socket(DeviceDefinition),
-}
-
-#[derive(Debug, Clone)]
 pub enum Bulb {
     TunableWhite(DeviceDefinition),
     DimmableWhite(DeviceDefinition),
     Rgb(DeviceDefinition),
 }
 
-impl Device {
-    pub fn new(
-        device_type: DeviceType,
-        features: Option<DeviceFeatures>,
-        descriptor: Option<DeviceDescriptor>,
-        config: Option<DeviceConfig>,
-    ) -> Self {
-        match device_type {
-            DeviceType::BulbTW => Device::Bulb(Bulb::TunableWhite(DeviceDefinition {
-                features: if let Some(features) = features {
-                    features
-                } else {
-                    DeviceFeatures {
-                        hue: false,
-                        color_temp: true,
-                        effects: true,
-                        dimming: true,
-                        dual_head: false,
-                    }
-                },
-                descriptor: if let Some(descriptor) = descriptor {
-                    descriptor
-                } else {
-                    DeviceDescriptor {
-                        module_name: None,
-                        color_temp: None,
-                        white_channels: None,
-                        white_to_color_ratio: None,
-                        firmware_version: None,
-                        type_id_index: None,
-                    }
-                },
-                config: if let Some(config) = config {
-                    config
-                } else {
-                    DeviceConfig {
-                        address: None,
-                        mac: None,
-                        name: None,
-                        id: Some(Uuid::new_v4()),
-                        state: DeviceState::Disabled,
-                        speed: None,
-                        scene: None,
-                        total_intensity: Some(Intensity::from_percent(0)),
-                        color_data: Some(ColorType::White(WhiteType::Tunable(WhiteTunableType {
-                            temp: ColorTemp::new(
-                                5000,
-                                ColorTempSpace {
-                                    min_temp: 1500,
-                                    max_temp: 8000,
-                                },
-                            ),
-                            intensity: Intensity::from_percent(0),
-                        }))),
-                    }
-                },
-            })),
-            DeviceType::BulbDW => Device::Bulb(Bulb::DimmableWhite(DeviceDefinition {
-                features: if let Some(features) = features {
-                    features
-                } else {
-                    DeviceFeatures {
-                        hue: false,
-                        color_temp: false,
-                        effects: false,
-                        dimming: true,
-                        dual_head: false,
-                    }
-                },
-                descriptor: if let Some(descriptor) = descriptor {
-                    descriptor
-                } else {
-                    DeviceDescriptor {
-                        module_name: None,
-                        color_temp: None,
-                        white_channels: None,
-                        white_to_color_ratio: None,
-                        firmware_version: None,
-                        type_id_index: None,
-                    }
-                },
-                config: if let Some(config) = config {
-                    config
-                } else {
-                    DeviceConfig {
-                        address: None,
-                        mac: None,
-                        name: None,
-                        id: Some(Uuid::new_v4()),
-                        state: DeviceState::Disabled,
-                        speed: None,
-                        scene: None,
-                        total_intensity: Some(Intensity::from_percent(0)),
-                        color_data: Some(ColorType::White(WhiteType::Variable(
-                            WhiteVariableType {
-                                current: WhiteStaticType::Mixed,
-                                ratio: 0.5,
-                                intensity: Intensity::from_percent(0),
-                            },
-                        ))),
-                    }
-                },
-            })),
-            DeviceType::BulbRGB => Device::Bulb(Bulb::Rgb(DeviceDefinition {
-                features: if let Some(features) = features {
-                    features
-                } else {
-                    DeviceFeatures {
-                        hue: true,
-                        color_temp: true,
-                        effects: true,
-                        dimming: true,
-                        dual_head: false,
-                    }
-                },
-                descriptor: if let Some(descriptor) = descriptor {
-                    descriptor
-                } else {
-                    DeviceDescriptor {
-                        module_name: None,
-                        color_temp: None,
-                        white_channels: None,
-                        white_to_color_ratio: None,
-                        firmware_version: None,
-                        type_id_index: None,
-                    }
-                },
-                config: if let Some(config) = config {
-                    config
-                } else {
-                    DeviceConfig {
-                        address: None,
-                        mac: None,
-                        name: None,
-                        id: Some(Uuid::new_v4()),
-                        state: DeviceState::Disabled,
-                        speed: None,
-                        scene: None,
-                        total_intensity: Some(Intensity::from_percent(0)),
-                        color_data: Some(ColorType::Rgb(Rgb {
-                            red: Intensity::from_percent(0),
-                            green: Intensity::from_percent(0),
-                            blue: Intensity::from_percent(0),
-                        })),
-                    }
-                },
-            })),
-            DeviceType::Socket => Device::Socket(DeviceDefinition {
-                features: if let Some(features) = features {
-                    features
-                } else {
-                    DeviceFeatures {
-                        hue: false,
-                        color_temp: false,
-                        effects: false,
-                        dimming: false,
-                        dual_head: false,
-                    }
-                },
-                descriptor: if let Some(descriptor) = descriptor {
-                    descriptor
-                } else {
-                    DeviceDescriptor {
-                        module_name: None,
-                        color_temp: None,
-                        white_channels: None,
-                        white_to_color_ratio: None,
-                        firmware_version: None,
-                        type_id_index: None,
-                    }
-                },
-                config: if let Some(config) = config {
-                    config
-                } else {
-                    DeviceConfig {
-                        address: None,
-                        mac: None,
-                        name: None,
-                        id: Some(Uuid::new_v4()),
-                        state: DeviceState::Disabled,
-                        speed: None,
-                        scene: None,
-                        total_intensity: None,
-                        color_data: None,
-                    }
-                },
-            }),
-        }
-    }
-
-    pub fn from_descriptor(descriptor: DeviceDescriptor) -> Result<Self, Error> {
-        let mut device: Device = Device::new(DeviceType::BulbDW, None, None, None);
-        let descriptor_bind = descriptor.clone();
-
-        if let Some(name) = descriptor.module_name {
-            let identifier = name
-                .split("_")
-                .collect::<Vec<&str>>()
-                .get(1)
-                .ok_or(Error::new(DeviceTypeParseError {
-                    data: descriptor_bind.clone(),
-                    details: "Failed to find an identifier in the descriptor.".to_string(),
-                }))?
-                .clone();
-
-            if identifier.contains(DEVICE_OPTS.rgb) {
-                device = Device::new(DeviceType::BulbRGB, None, None, None);
-            } else if identifier.contains(DEVICE_OPTS.tunable_white) {
-                device = Device::new(DeviceType::BulbTW, None, None, None);
-            } else if identifier.contains(DEVICE_OPTS.socket) {
-                device = Device::new(DeviceType::Socket, None, None, None);
-            } else {
-                let effects = identifier.contains(DEVICE_OPTS.dual_head)
-                    || identifier.contains(DEVICE_OPTS.single_head);
-                let dual_head = identifier.contains(DEVICE_OPTS.dual_head);
-                let patch = OptionalDeviceFeatures {
-                    hue: None,
-                    color_temp: None,
-                    effects: Some(effects),
-                    dimming: None,
-                    dual_head: Some(dual_head),
-                };
-                device = device.patch_features(patch);
-            }
-        } else if let Some(type_id_index) = descriptor.type_id_index {
-            let device_type = KNOWN_TYPE_IDS
-                .get(type_id_index)
-                .ok_or(Error::new(DeviceTypeParseError {
-                    data: descriptor,
-                    details: "Failed finding a known type ID in the descriptor".to_string(),
-                }))?
-                .clone();
-            device = Device::new(device_type, None, None, None);
-            let patch = OptionalDeviceFeatures {
-                hue: None,
-                color_temp: None,
-                effects: Some(true),
-                dimming: None,
-                dual_head: None,
-            };
-            device = device.patch_features(patch);
-        }
-
-        if let Some(color_temp) = descriptor_bind.color_temp {
-            let descriptor = OptionalDeviceDescriptor {
-                module_name: None,
-                color_temp: Some(color_temp),
-                firmware_version: None,
-                white_channels: None,
-                white_to_color_ratio: None,
-                type_id_index: None,
-            };
-
-            device = device.patch_descriptor(descriptor);
-        } else if device.get_type() == DeviceType::BulbRGB
-            || device.get_type() == DeviceType::BulbTW
-        {
-            return Err(Error::new(DeviceColorTempParseError {
-                data: device.get_definition().descriptor,
-                details: "Bulb type should include color temp data in the descriptor.".to_string(),
-            }));
-        }
-
-        return Ok(device);
-    }
-
-    pub fn patch_descriptor(self: Self, descriptor: OptionalDeviceDescriptor) -> Self {
-        let mut definition = self.get_definition();
-        let device_type = self.get_type();
-        definition.descriptor.apply_options(descriptor);
-
-        Device::new(
-            device_type,
-            Some(definition.features),
-            Some(definition.descriptor),
-            Some(definition.config),
-        )
-    }
-
-    pub fn patch_features(self: Self, features: OptionalDeviceFeatures) -> Self {
-        let mut definition = self.get_definition();
-        let device_type = self.get_type();
-        definition.features.apply_options(features);
-
-        Device::new(
-            device_type,
-            Some(definition.features),
-            Some(definition.descriptor),
-            Some(definition.config),
-        )
-    }
-
-    pub fn patch_config(self: Self, config: OptionalDeviceConfig) -> Self {
-        let mut definition = self.get_definition();
-        let device_type = self.get_type();
-        definition.config.apply_options(config);
-
-        Device::new(
-            device_type,
-            Some(definition.features),
-            Some(definition.descriptor),
-            Some(definition.config),
-        )
-    }
-
-    pub fn get_definition(self: &Self) -> DeviceDefinition {
-        match self {
-            Self::Socket(definition) => definition.clone(),
-            Self::Bulb(bulb) => match bulb {
-                Bulb::DimmableWhite(definition) => definition.clone(),
-                Bulb::TunableWhite(definition) => definition.clone(),
-                Bulb::Rgb(definition) => definition.clone(),
-            },
-        }
-    }
-
-    pub fn get_type(self: &Self) -> DeviceType {
-        match self {
-            Self::Socket(_) => DeviceType::Socket,
-            Self::Bulb(bulb) => match bulb {
-                Bulb::TunableWhite(_) => DeviceType::BulbTW,
-                Bulb::DimmableWhite(_) => DeviceType::BulbDW,
-                Bulb::Rgb(_) => DeviceType::BulbRGB,
-            },
-        }
-    }
-}
-
 #[derive(Debug, Clone)]
 pub struct NetworkConfig {
-    timeout: Duration,
-    max_sent_datagrams: u8,
-    first_send_interval: Duration,
-    max_backoff: u8,
-    keepalive: Duration,
-    port: u16,
-}
-
-#[derive(Debug, Clone)]
-pub enum GroupType {
-    Group,
-    Room,
-    House,
-}
-#[derive(Debug, Clone)]
-pub struct DeviceGroup {
-    pub devices: Vec<Device>,
-    pub name: String,
-    pub id: Uuid,
-    pub group_type: GroupType,
-    pub network_config: NetworkConfig,
-}
-
-impl DeviceGroup {
-    pub fn new(
-        name: String,
-        group_type: GroupType,
-        devices: Option<Vec<Device>>,
-        id: Option<Uuid>,
-        network_config: Option<NetworkConfig>,
-    ) -> Self {
-        let devices_holder: Vec<Device>;
-        let id_holder: Uuid;
-        if let Some(devices) = devices {
-            devices_holder = devices;
-        } else {
-            devices_holder = Vec::new();
-        }
-
-        if let Some(id) = id {
-            id_holder = id;
-        } else {
-            id_holder = Uuid::new_v4();
-        }
-
-        return DeviceGroup {
-            devices: devices_holder,
-            name: name,
-            id: id_holder,
-            group_type: group_type,
-            network_config: if let Some(network_config) = network_config {
-                network_config
-            } else {
-                NetworkConfig {
-                    timeout: DEFAULT_NETWORK_CONFIG.timeout,
-                    max_sent_datagrams: DEFAULT_NETWORK_CONFIG.max_sent_datagrams,
-                    first_send_interval: DEFAULT_NETWORK_CONFIG.first_send_interval,
-                    max_backoff: DEFAULT_NETWORK_CONFIG.max_backoff,
-                    keepalive: DEFAULT_NETWORK_CONFIG.keepalive,
-                    port: DEFAULT_NETWORK_CONFIG.port,
-                }
-            },
-        };
-    }
+    pub timeout: Duration,
+    pub max_sent_datagrams: u8,
+    pub first_send_interval: Duration,
+    pub max_backoff: u8,
+    pub keepalive: Duration,
+    pub port: u16,
 }
--- src/wiz_errors.rs
+++ src/wiz_errors.rs
@@ -1,91 +1,24 @@
-use super::model::{ColorTempSpace, DeviceDescriptor};
-use std::error::Error;
-use std::fmt;
+use super::model::DeviceDescriptor;
+use thiserror::Error;
 
-#[derive(Debug, Clone)]
-pub struct SceneIDError {
-    pub given_id: u32,
-    pub details: String,
+#[derive(Error, Debug)]
+pub enum SceneError {
+    #[error("Bad scene ID given. Expected 1-32 or 1000, received {0}")]
+    IDError(u32),
+    #[error("Bad scene name given. Given {0}")]
+    NameError(String),
 }
 
-impl fmt::Display for SceneIDError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "Given Scene ID {}. Error: {}",
-            self.given_id, self.details
-        )
-    }
+#[derive(Error, Debug)]
+pub enum DeviceError {
+    #[error(transparent)]
+    Parse(ParseError),
 }
 
-impl Error for SceneIDError {
-    fn description(&self) -> &str {
-        &self.details
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct SceneNameError {
-    pub given_name: String,
-    pub details: String,
-}
-
-impl fmt::Display for SceneNameError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "Given Scene Name {}. Error: {}",
-            self.given_name, self.details
-        )
-    }
-}
-
-impl Error for SceneNameError {
-    fn description(&self) -> &str {
-        &self.details
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct DeviceTypeParseError {
-    pub data: DeviceDescriptor,
-    pub details: String,
-}
-
-impl fmt::Display for DeviceTypeParseError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "Device type could not be determined from the given descriptor: {:#?}\n {}",
-            self.data, self.details
-        )
-    }
-}
-
-impl Error for DeviceTypeParseError {
-    fn description(&self) -> &str {
-        &self.details
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct DeviceColorTempParseError {
-    pub data: DeviceDescriptor,
-    pub details: String,
-}
-
-impl fmt::Display for DeviceColorTempParseError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(
-            f,
-            "Failed to find a color temperature space in the given descriptor: {:#?}\n {}",
-            self.data, self.details
-        )
-    }
-}
-
-impl Error for DeviceColorTempParseError {
-    fn description(&self) -> &str {
-        &self.details
-    }
+#[derive(Error, Debug)]
+pub enum ParseError {
+    #[error("Unable to determine the device type for the described device.\n Descriptor: {0:#?}")]
+    UndefinedType(DeviceDescriptor),
+    #[error("Failed to find a expected color temperature space in the given descriptor.\n Descriptor: {0:#?}")]
+    ColorTemp(DeviceDescriptor),
 }
